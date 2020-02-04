41ec04f8653b5e9ef6f9bc5707e951d48e558ad3
Pedro Bufulin <pedro.almeida@ufu.br>
2020-02-04 11:23:27 -0300

    listagem de agendamento de usuarios

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,12 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 export default routes;
