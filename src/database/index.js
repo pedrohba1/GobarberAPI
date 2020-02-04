4af44613ffe5d8654a499783f02eaf352baa72bd
Pedro Bufulin <pedro.almeida@ufu.br>
2020-02-04 12:21:55 -0300

    confiugraçao do mongo db

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointments from '../app/models/Appointment';
+import mongoose from 'mongoose';
 
 const models = [User, File, Appointments];
 
@@ -19,6 +20,13 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            { useNewUrlParser: true, useFindAndModify: true }
+        );
     }
 }
 
