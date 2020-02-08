6697077d456d8e7fbfaf15eaf2ffb482fac42056
Pedro Bufulin <pedro.almeida@ufu.br>
2020-02-08 00:10:36 -0300

    variaveis de ambiente

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+import 'dotenv/config';
+import * as Sentry from '@sentry/node';
 import express from 'express';
+import Youch from 'youch';
 import path from 'path';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,11 +14,26 @@ class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middleWares();
         this.routes();
+        this.exceptionHandler();
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            if (process.env.NODE_ENV === 'development') {
+                const errors = await new Youch(err, req).toJSON();
+                return res.status(500).json(errors);
+            }
+            return res.status(500).json({ error: 'internal server error' });
+        });
     }
 
     middleWares() {
+        this.server.use(Sentry.Handlers.requestHandler());
+
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -22,6 +43,7 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
     }
 }
 
